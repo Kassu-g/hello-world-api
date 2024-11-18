b94f21a309683f0a4c1c933055938d7838728690
kasper666g <64731263+kasper666g@users.noreply.github.com>
2024-11-18 18:20:17 +0200

    server update

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/h', (req, res) => {
   res.json({ msg: 'Hello world!' });
 });
 
+app.get('/echo/:id', (req, res) => {
+  const { id } = req.params;  
+  res.json({ id });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
