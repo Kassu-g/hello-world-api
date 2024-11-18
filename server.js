77f080bccd2c3e638201c0893cca483760181452
kasper666g <64731263+kasper666g@users.noreply.github.com>
2024-11-18 18:50:23 +0200

    update

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/h', (req, res) => {
+app.get('/hello', (req, res) => {
   res.json({ msg: 'Hello world!' });
 });
 
 app.get('/echo/:id', (req, res) => {
-  const { id } = req.params;  
-  res.json({ id });
+  const { huuto } = req.params;  
+  res.json({ huuto });
 });
 app.use(express.json());
 app.post('/sum', (req, res) => {
     const { luvut } = req.body;
   
-    if (!Array.isArray(luvut) || luvut.some(isNaN)) {
-      return res.status(400).json({ error: 'Ei ollut numeroita.' });
-    }
   
     const sum = luvut.reduce((summa, luku) => summa + luku, 0);
   
