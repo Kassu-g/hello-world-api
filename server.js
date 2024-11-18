24566c0c08b8e28e8daf3a314e75127e91b4adb2
kasper666g <64731263+kasper666g@users.noreply.github.com>
2024-11-18 18:44:39 +0200

    server update

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.get('/echo/:id', (req, res) => {
   const { id } = req.params;  
   res.json({ id });
 });
+app.use(express.json());
+app.post('/sum', (req, res) => {
+    const { luvut } = req.body;
+  
+    if (!Array.isArray(luvut) || luvut.some(isNaN)) {
+      return res.status(400).json({ error: 'Ei ollut numeroita.' });
+    }
+  
+    const sum = luvut.reduce((summa, luku) => summa + luku, 0);
+  
+    res.json({ sum });
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
